a2a2c3d271b1e40615bda99b4b3670ee5fb6fd85
Bluejee <balakrishnaprabhu1999@gmail.com>
2023-03-30 18:57:22 +0530

    Got the Visualisation working.

--- Environment.py
+++ Environment.py
@@ -35,14 +35,19 @@ class World:
     The layer input while initialising the class also contains additional information such as layer type and color which
     can be used for visualisation
 
+
+    The layer for a BlockElement is stored as an empty numpy array with each element being a [x,y].
+    The layer for a FloatElement is stored as a NxM matrix with each element storing the value for the cell.
+
+
     """
 
     def __init__(self, layer_data: dict, r_length=30, c_length=30):
         """
 
         :param layer_data: data of the form
-        {'layer_label': ['Layer_type(Block/Float)', '#Hex_code_for_color', 'Path_for_sprite_image/None',
-        'In case of Float a max value to calculate %']}
+        {'layer_label': ['Layer_type(Block/Float)', (R, G, B), 'Path_for_sprite_image/None',
+        In case of Float a max value to calculate %]}
         :param r_length: number of rows of the world.
         :param c_length: number of columns of the world.
         """
@@ -50,9 +55,14 @@ class World:
         self.c_length = c_length  # no of columns
         self.layer_data = layer_data
         self.layers = {}
+
         for label in self.layer_data:
-            self.layers[label] = np.zeros([self.r_length, self.c_length])  # formation of one layer
-        # r represents the vertical(row number) and c the horizontal(column number).
+            if layer_data[label][0] == 'Block':
+                self.layers[label] = np.array([], dtype=int)  # formation of one layer of Block
+
+            elif layer_data[label][0] == 'Float':
+                self.layers[label] = np.zeros([self.r_length, self.c_length])  # formation of one layer
+                # r represents the vertical(row number) and c the horizontal(column number).
 
 
 class Realise:
@@ -61,13 +71,14 @@ class Realise:
     """
 
     def __init__(self, world: World, loop_step, clock: Clock, cell_size=20, border_size=2, border_color=(255, 0, 0),
-                 clock_color=(56, 74, 12)):
+                 clock_color=(56, 74, 12), menu_background=(0, 255, 0), sim_background=(0, 0, 255)):
+
         pygame.init()
         # Simulation Variables
         self.world = world
         self.loop_step = loop_step
         self.clock = clock
-        self.state = "Pause"
+        self.state = "Pause"  # The visualisation starts at the paused state.
 
         # Visualisation Variables
         self.pyclock = pygame.time.Clock()
@@ -77,7 +88,7 @@ class Realise:
         self.border_color = border_color
 
         self.sim_width = (self.cell_size * self.world.c_length)  # width = no of columns * cell_size
-        self.sim_height = (self.cell_size * self.world.r_length)  # height = no of rows * cell_size
+        self.sim_height = (self.cell_size * self.world.r_length)  # height = no of rows * celbackgroundl_size
 
         self.menu_width = int(self.sim_width / 3)
 
@@ -86,6 +97,11 @@ class Realise:
 
         self.clock_color = clock_color
 
+        # This creates a dictionary with keys being layer names and the value containing a class which contains
+        # information on how to display and if to display the layer.
+        self.display_layers = {layer: DisplayLayers(layer_data=self.world.layer_data[layer]) for layer in
+                               self.world.layer_data}
+
         # Surface and Display Variables
         # Screen
         self.screen = pygame.display.set_mode((self.screen_width, self.screen_height))
@@ -93,19 +109,28 @@ class Realise:
 
         # Menu
         self.menu_surf = pygame.Surface((self.menu_width, self.screen_height))
-        self.menu_surf.fill((0, 255, 0))
         self.menu_rect = self.menu_surf.get_rect(topright=(self.screen_width, 0))
+        self.menu_surf.fill(menu_background)
 
         # Simulation
         self.sim_surf = pygame.Surface((self.sim_width, self.sim_height))
-        self.sim_surf.fill((0, 0, 255))
         self.sim_rect = self.sim_surf.get_rect(topleft=(self.border_size, self.border_size))
+        self.sim_surf.fill(sim_background)
 
         # Clock/Step text
         self.clock_font = pygame.font.Font(None, 25)
 
     def draw_menu(self):
-        # Draw menu
+        """
+        This function draws the menu surface of the visualisation. it does the following.
+        1. It shows the simulation clock and allows the user to play and pause the simulation.
+        2. It allows the user to select which all layers are to be displayed.
+            2.1 This happens only if the number of layers can be displayed on the menu.
+            2.2 If the number is too large it can read from a file the list of layers to be displayed. (Future update*)
+
+        :return:
+        """
+        # Draw menu background
         self.screen.blit(self.menu_surf, self.menu_rect)
 
         # Clock Step
@@ -127,12 +152,49 @@ class Realise:
         self.screen.blit(prompt_surf, prompt_rect)
 
     def draw_sim(self):
-        # Draw screen
+        # This function draws the layers of the world on to the screen
+        # Draw simulation Background
         self.screen.blit(self.sim_surf, self.sim_rect)
 
+        # going through every layer
+        for layer_name in self.display_layers:
+            layer: DisplayLayers
+            # This is a type set. i.e. helping to know that the layer variable is of the DisplayLayers class
+            # Not at all necessary but helps in debugging and getting docs on the fly
+
+            layer = self.display_layers[layer_name]
+            if layer.active:
+                # taking the values from the world.
+                layer_values = self.world.layers[layer_name]
+
+                # We are now in the active layer.
+                # We create a surface for this layer so that we can have transparency working.
+                layer_surf = pygame.Surface((self.sim_width, self.sim_height), pygame.SRCALPHA)
+                # We can use the sim_rect to place the layer_surf as both are basically the same thing.
+
+                # We check if the layer is a block type or a float type and then act accordingly.
+                if layer.layer_type == 'Block':
+                    # Block type layer
+                    # We go through all the coordinates in the layer
+                    for [x, y] in layer_values:
+                        location = (x * self.cell_size, y * self.cell_size, self.cell_size, self.cell_size)
+                        layer.draw_cell(value=1, surface=layer_surf, location=location)
+                elif layer.layer_type == 'Float':
+                    # Block type layer
+                    # We go through all cells of the layer.
+                    for x in range(self.world.c_length):
+                        for y in range(self.world.r_length):
+                            # we are now at a cell in the layer
+                            # location and size to draw the cell = (x,y,w,h)
+                            location = (x * self.cell_size, y * self.cell_size, self.cell_size, self.cell_size)
+                            layer.draw_cell(value=layer_values[y, x], surface=layer_surf, location=location)
+                            # layer.draw_cell(value=np.random.randint(100), surface=layer_surf, location=location)
+
+                # Now that all cells have been drawn, we blit the layer_surf on to the screen
+                self.screen.blit(layer_surf, self.sim_rect)
+
     def draw(self):
-        # This function draws the layers of the world on to the screen
-        # This also creates a list of layers for the user to click on to add or remove layer displays.
+        # This function draws the menu and the simulation surfaces.
         self.draw_menu()
         self.draw_sim()
         return
@@ -144,6 +206,12 @@ class Realise:
             self.state = "Play"
 
     def loop(self):
+        # Running the draw and update functions once to show initial state.
+        # Running the draw loop to create all the necessary layouts and surfaces
+        self.draw()
+        # updating the changes to the screen and limiting to 60 fps
+        pygame.display.update()
+
         while True:
             # Checking events in pygame.
             for event in pygame.event.get():
@@ -155,16 +223,71 @@ class Realise:
                         self.switch_state()
 
             if self.state == "Play":
+                # Running the draw loop to create all the necessary layouts and surfaces
+                self.draw()
+                # updating the changes to the screen and limiting to 60 fps
+                pygame.display.update()
+
                 # Running one step of the loop as provided in the definition.
                 # This is defined by the creator according to the needs of the simulation.
-                self.loop_step()
+                self.loop_step(self.world)
 
                 # updating the clock as one step is completed.
                 self.clock.next_step()
 
-            # Running the draw loop to create all the necessary layouts and surfaces
-            self.draw()
-            # updating the changes to the screen and limiting to 60 fps
-            pygame.display.update()
-
             self.pyclock.tick(60)
+
+
+class DisplayLayers:
+    def __init__(self, layer_data):
+        # if self.layer_type == 'Block':
+        #     self.element = BlockElement(layer_data[1], layer_data[2])
+        # elif self.layer_type == 'Float':
+        #     self.element = FloatElement(layer_data[1], layer_data[2], layer_data[3])
+        # else:
+        #     print("ERROR!!!")
+        #     print("This should not have happened.")
+        #     print("It seems like you have misrepresented a layer.")
+        #     print("Check the Definition of the World and check if all layers are either Block or Float.")
+        #     exit()
+
+        self.layer_type = layer_data[0]
+        self.color = layer_data[1]
+        self.sprite_image = layer_data[2]
+        if self.layer_type == 'Float':
+            self.max_value = layer_data[3]
+
+        self.active = 1
+
+    def draw_cell(self, value, surface, location):
+        if self.layer_type == 'Block':
+            if value == 1:
+                # draw the block
+                pygame.draw.rect(surface, self.color, location)
+
+        elif self.layer_type == 'Float':
+            # draw the Float
+            # setting transparency value
+            transparency = int((value / self.max_value) * 255)
+            pygame.draw.rect(surface, (*self.color, transparency), location)
+
+        else:
+            print("ERROR!!!")
+            print("This should not have happened.")
+            print("It seems like you have misrepresented a layer.")
+            print("Check the Definition of the World and check if all layers are either Block or Float.")
+            exit()
+
+# class BlockElement:
+#
+#     def __init__(self, color, sprite_image):
+#         self.color = color
+#         self.sprite_image = sprite_image
+#
+#
+# class FloatElement:
+#
+#     def __init__(self, color, sprite_image, max_value):
+#         self.color = color
+#         self.sprite_image = sprite_image
+#         self.max_value = max_value
--- Main_Code.py
+++ Main_Code.py
@@ -12,32 +12,38 @@ of simulations.
 # It is not recommended that the number of layers be more than 10 as it will cause errors within the visualisation.
 # The simulation will however work fine. just that the option for selecting layers will be disabled
 
-layers = {'Agent': ['Block', '#619130', 'None'], 'Pheromone': ['Float', '#bd7c22', 'None'],
-          'Home': ['Block', '#2227bd', 'None']}
+layers = {'Agent': ['Block', (255, 0, 0), 'None'], 'Pheromone': ['Float', (209, 126, 17), 'None', 100],
+          'Home': ['Block', (214, 103, 191), 'None']}
 
-world = World(layer_data=layers, r_length=30, c_length=30)
+w = World(layer_data=layers, r_length=30, c_length=30)
+
+# Setting up the Agent and Home
+w.layers['Agent'] = [[3, 5], [7, 9], [22, 28]]
+w.layers['Home'] = [[15, 15], [14, 14], [15, 14]]
 
 # Initialise the clock.
 clock = Clock()
 
 
 # Create one step of the event loop that is to happen. i.e. how the world changes in one step.
-def one_loop_step():
+
+
+def one_loop_step(world):
     """
     This function is passed to the realise class to be run everytime the loop iterates.
     Basically this function is the entire set of changes that are to happen to the world.
     :return:
     """
     # for random data for trial.
-    if np.random.randint(60) == 1:
-        # 1 in 60 probability to make a change
-        layer = world.layers['Agent']
-        layer[np.random.randint(30), np.random.randint(30)] = 1
+    # if np.random.randint(60) == 1:
+    #     # 1 in 60 probability to make a change
+    #     layer = world.layers['Agent']
+    #     layer[np.random.randint(30), np.random.randint(30)] = 1
     return
 
 
 # Initialise the realisation/ Gods Perspective
-realise = Realise(world=world, loop_step=one_loop_step, clock=clock)
+realise = Realise(world=w, loop_step=one_loop_step, clock=clock)
 
 # Now that all the classes have been initialised.
 realise.loop()
