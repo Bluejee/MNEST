f6c47f3a69f280623899ed5f99fa4561fe22056d
Bluejee <balakrishnaprabhu1999@gmail.com>
2023-04-03 01:39:09 +0530

    Updated some codes for test runs.

--- Main_Code.py
+++ Main_Code.py
@@ -1,4 +1,8 @@
 from Environment import World, Clock, Realise
+from Entities import Agent, Essence
+from Laws import *
+import numpy as np
+import random
 
 """
 This is the main code file. The following is a sample template that can be modified inorder to create any type 
@@ -16,15 +20,15 @@ layers = {'Pheromone': ['Float', (209, 126, 17), 'None', 100],
 w = World(layer_data=layers, r_length=30, c_length=30)
 
 # Setting up the Agent and Home
-w.layers['Agent'] = [[0, 0], [7, 9], [22, 28]]
+agent_list = [Agent(w, 'Agent', Vector2(i % 30, i % 30)) for i in range(50)]
 w.layers['Home'] = [[15, 15], [14, 14], [15, 14]]
-
+pheromone = Essence(w, 'Pheromone', 0, 3)
 # Initialise the clock.
 clock = Clock()
 
 # Create one step of the event loop that is to happen. i.e. how the world changes in one step.
 
-parameters = [w]
+parameters = [w, agent_list, pheromone]
 
 
 # params is a list of variables defined in the main code that are set up during initialisation that are required for
@@ -34,25 +38,36 @@ parameters = [w]
 # Inorder to preserve the names of the variables, we copy the list (the right-hand side of the params equations).
 # and paste it into the loop to access the same parameters while being passed through the realise module.
 
+
 def one_loop_step(loop_parameters: list):
     """
     This function is passed to the realise class to be run everytime the loop iterates.
     Basically this function is the entire set of changes that are to happen to the world.
     :return:
     """
-    [w] = loop_parameters
+    # [w, agent_list, pheromone] = loop_parameters
+    for agent in agent_list:
+        agent.move()
 
-    # for random data for trial.
-    # if np.random.randint(60) == 1:
-    #     # 1 in 60 probability to make a change
-    #     layer = world.layers['Agent']
-    #     layer[np.random.randint(30), np.random.randint(30)] = 1
+    for agent in agent_list:
+        if np.random.randint(2) == 1:
+            agent.direction = random.choice([RIGHT, LEFT, UP, DOWN])
+        agent.move()
+        # print(agent.position.y, agent.position.x)
+        # using random and not np.random to use objects and not a np array.
+        loc = w.layers['Pheromone'][int(agent.position.y), int(agent.position.x)]
+        if loc < 80:
+            w.layers['Pheromone'][int(agent.position.y), int(agent.position.x)] += 20
+        # print(w.layers['Pheromone'][0])
+
+    pheromone.decay()
+
     return
 
 
 # Initialise the realisation/ Gods Perspective
 realise = Realise(world=w, loop_step=one_loop_step, loop_parameters=parameters,
                   clock=clock, sim_background=(89, 187, 247))
-
+realise.frame_rate_cap = 60
 # Now that all the classes have been initialised.
 realise.loop()
