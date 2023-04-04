35e472a7cde421dd82242347c5d8f3058077ae2f
Bluejee <balakrishnaprabhu1999@gmail.com>
2023-04-04 17:01:42 +0530

    Added Directions and methods to change directions.

--- Laws.py
+++ Laws.py
@@ -1,6 +1,43 @@
 from pygame.math import Vector2
 
-RIGHT = Vector2(1, 0)
-LEFT = Vector2(-1, 0)
-UP = Vector2(0, 1)
-DOWN = Vector2(0, -1)
+E = Vector2(1, 0)
+W = Vector2(-1, 0)
+N = Vector2(0, 1)
+S = Vector2(0, -1)
+NE = Vector2(1, 1)
+NW = Vector2(-1, 1)
+SE = Vector2(1, -1)
+SW = Vector2(-1, -1)
+
+DIRECTIONS = [E,
+              NE,
+              N,
+              NW,
+              W,
+              SW,
+              S,
+              SE]
+
+
+def front(direction):
+    return DIRECTIONS[DIRECTIONS.index(direction)]
+
+
+def front_left(direction):
+    return DIRECTIONS[(DIRECTIONS.index(direction) + 1) % 8]
+
+
+def front_right(direction):
+    return DIRECTIONS[(DIRECTIONS.index(direction) - 1) % 8]
+
+
+def back(direction):
+    return -direction
+
+
+def back_left(direction):
+    return front_right(back(direction))
+
+
+def back_right(direction):
+    return front_left(back(direction))
