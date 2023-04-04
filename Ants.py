ebdf1ebf7d5693c7b3f42e6a4acffb1b679711be
Bluejee <balakrishnaprabhu1999@gmail.com>
2023-04-04 14:30:48 +0530

    Added outline functions of the actions and tests to check actions.

--- Ants.py
+++ Ants.py
@@ -15,8 +15,10 @@ Note : Maybe start custom variables with an _ or some identifier to prevent acci
 
 class Ant(Agent):
 
+    # Initialise the parent class. Make sure to initialise it with the child as self.
     def __init__(self, world, layer_name, position=Vector2(0, 0)):
-        super().__init__(world, layer_name, position)
+        super().__init__(world=world, layer_name=layer_name, child=self, position=position,
+                         action_list=['move_random, go_home, go_target, drop_home, drop_target'])
 
     def drop_pheromone(self, pheromone_type, quantity):
         pheromone_type = 'Pheromone_' + pheromone_type  # for simplicity
@@ -27,6 +29,22 @@ class Ant(Agent):
         if loc > 100:
             self.world.layers[pheromone_type][int(self.position.y), int(self.position.x)] = 100
             # print(w.layers['Pheromone'][0])
+
+    def move_random(self):
+        self.direction = random.choice([RIGHT, LEFT, UP, DOWN])
+        self.move()
+
+    def go_home(self):
+        pass
+
+    def go_target(self):
+        pass
+
+    def drop_home(self):
+        self.drop_pheromone(pheromone_type='Home', quantity=100)
+
+    def drop_target(self):
+        self.drop_pheromone(pheromone_type='Target', quantity=100)
 
 
 # Setting up the Visualiser.
@@ -39,27 +57,29 @@ class Visualise(Realise):
         # The simulation will however work fine. just that the option for selecting layers will be disabled
 
         # Create the necessary layers.
-        layers = {'Pheromone_B': ['Float', (209, 126, 17), 'None', 100],
-                  'Pheromone_A': ['Float', (126, 209, 17), 'None', 100],
+        layers = {'Pheromone_Home': ['Float', (209, 126, 17), 'None', 100],
+                  'Pheromone_Target': ['Float', (126, 209, 17), 'None', 100],
                   'Home': ['Block', (214, 103, 191), 'None'],
+                  'Target': ['Block', (214, 191, 103), 'None'],
                   'Ants': ['Block', (255, 0, 0), 'Stock_Images/ant_sq.png']}
 
-        # Initialise the parent class.
+        # Initialise the parent class. Make sure to initialise it with the child as self.
         # Adjust set parameters
-        # sim_background = (255, 255, 255)
-        super().__init__(world=World(layer_data=layers, r_length=30, c_length=30), frame_rate_cap=60, cell_size=20,
-                         sim_background=(255, 255, 255))
+        super().__init__(world=World(layer_data=layers, r_length=30, c_length=30), child=self, frame_rate_cap=60,
+                         cell_size=25, sim_background=(255, 255, 255))
         # Set up the new variables and performing initial setups.
-        self.ant_list = [Ant(self.world, 'Ants', Vector2(i % 30, i % 30)) for i in range(2)]
+        self.ant_list = [Ant(self.world, 'Ants', Vector2(i % 30, i % 30)) for i in range(5)]
         # self.ant_list = [Ant(self.world, 'Ants', Vector2(0, 0)), Ant(self.world, 'Ants', Vector2(5, 5))]
         self.world.layers['Home'] = [[15, 15], [14, 14], [15, 14], [14, 15]]
-        self.pheromone_a = Essence(self.world, 'Pheromone_A', decay_rate=1)
-        self.pheromone_b = Essence(self.world, 'Pheromone_B', decay_rate=3)
+        self.world.layers['Target'] = [[10, 10], [9, 9], [10, 9], [9, 10]]
+        self.pheromone_a = Essence(self.world, 'Pheromone_Home', decay_rate=1)
+        self.pheromone_b = Essence(self.world, 'Pheromone_Target', decay_rate=3)
         # Do not add any variables after calling the loop. it will cause object has no attribute error when used.
-        self.loop(self.one_loop_step)
+        # self.loop(self.one_loop_step)
+        self.loop()
 
     # Create one step of the event loop that is to happen. i.e. how the world changes in one step.
-    def one_loop_step(self):
+    def loop_step(self):
         """
         This function is passed to the realise class to be run everytime the loop iterates.
         Basically this function is the entire set of changes that are to happen to the world.
@@ -69,12 +89,14 @@ class Visualise(Realise):
             # if np.random.randint(2) == 1:
             # using random and not np.random to use objects and not a np array.
             # ant.direction = random.choice([RIGHT, LEFT, UP, DOWN])
-            ant.move()
-            if ant.position.y == 0:
-                ant.drop_pheromone('A', 100)
-            else:
-                ant.drop_pheromone('B', 100)
-
+            if self.clock.time_step < 1000:
+                if ant.position.y % 2 == 0:
+                    ant.selected_action = 'drop_target'
+                else:
+                    ant.selected_action = 'drop_home'
+                ant.perform_action()
+                ant.selected_action = 'move_random'
+                ant.perform_action()
         self.pheromone_a.disperse()
         self.pheromone_b.disperse()
         return
