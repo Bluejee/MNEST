d88d75b0397f481ffaee678e0022fc8756783e61
Bluejee <balakrishnaprabhu1999@gmail.com>
2023-04-08 16:33:33 +0530

    Updating the script file for testing.

--- script.sh
+++ script.sh
@@ -1,1 +1,19 @@
-python3 Ants.py --start_as='Play' --max_steps=1000 --sim_name='Hope_this_works_exam' --min_exploration=0.05 --exploration_rate=0.9 --exploration_decay=0.0001 --learning_rate=0.4 --discounted_return=0.85
+ python Ants.py --start_as='Play' --sim_name='trial_1' --learning_rate=0.1
+
+ python Ants.py --start_as='Play' --sim_name='trial_2' --learning_rate=0.2
+
+ python Ants.py --start_as='Play' --sim_name='trial_3' --learning_rate=0.3
+
+ python Ants.py --start_as='Play' --sim_name='trial_4' --learning_rate=0.4
+
+ python Ants.py --start_as='Play' --sim_name='trial_5' --learning_rate=0.5
+
+ python Ants.py --start_as='Play' --sim_name='trial_6' --learning_rate=0.6
+
+ python Ants.py --start_as='Play' --sim_name='trial_7' --learning_rate=0.7
+
+ python Ants.py --start_as='Play' --sim_name='trial_8' --learning_rate=0.8
+
+ python Ants.py --start_as='Play' --sim_name='trial_9' --learning_rate=0.9
+
+ python Ants.py --start_as='Play' --sim_name='trial_10' --learning_rate=0.9
